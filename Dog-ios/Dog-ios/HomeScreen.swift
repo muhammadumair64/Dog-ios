51cdea9de6f1e69649a7ca8c327a4631c335d159
Muhammad Umair <84626460+muhammadumair64@users.noreply.github.com>
2024-11-17 01:27:59 +0500

    main card done

--- Dog-ios/Dog-ios/HomeScreen.swift
+++ Dog-ios/Dog-ios/HomeScreen.swift
@@ -6,23 +6,89 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct HomeScreen: View {
     var userName: String
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack {
-            Text("Welcome, \(userName)!")
-                .font(.largeTitle)
-                .padding()
-                .onTapGesture {
-                                dismiss() // Dismisses the current screen (pops the navigation stack)
-                            }
-                        
-            
-            // Additional content for HomeScreen
-        }
+        
+                   ZStack(alignment: .top){
+                       VStack{
+                           HStack{
+                               Image(
+                                   ImageResource.menu
+                               ).frame(width: 20,height: 20)
+                               
+                               Text("Dog Translator")
+                                   .font(.system(size: 20))
+                                   .padding(.leading,40)
+                                   .bold()
+                               
+                           }
+                               .frame(maxWidth: .infinity,alignment: .leading)
+                               .padding(.horizontal,20)
+                       
+                           VStack(alignment: .leading) {
+                               
+                                   Text("Dog Translator")
+                                       .foregroundColor(.white)
+                                       .font(.system(size: 18))
+                                       .bold()
+                                       .padding(.bottom,5)
+                                   
+                                   
+                                   Text("Unlock Secret language of dog")
+                                       .foregroundColor(.white)
+                                       .font(.system(size: 14))
+                                       .multilineTextAlignment(.center)
+                                       .padding(.bottom,10)
+                               
+                               Text("Translate Now")
+                                   .frame(width: 130,height: 40)
+                                   .foregroundColor(Color(.primary))
+                                   .background(Color.white)
+                                   .cornerRadius(15)
+                                   .font(.system(size: 14))
+                                   .bold()
+                                   .multilineTextAlignment(.center)
+                               
+                                   
+                           }.padding(.horizontal,30)
+                           .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight: 145,alignment: .leading)
+                                       .clipped()
+                                       .background(
+                                           Image(ImageResource.homeCard)
+                                                   .resizable()
+                                                   .scaledToFill() )
+                                       .cornerRadius(20)
+                                       .padding(.horizontal,15)
+                                       .padding(.top,20)
+                           
+                           
+                       
+                       }.ignoresSafeArea()
+                           .frame(
+                               maxWidth: .infinity,
+                               maxHeight: .infinity,
+                               alignment: .top
+                           )
+                           .padding(
+                               .vertical,
+                               20
+                           )
+                       // Main Card Lottie
+                       HStack{
+                           LottieView(animation: .named("main_screen_lottie"))
+                               .playing()
+                               .looping()
+                               .frame(width: 160)
+                           
+                       }.frame(maxWidth: .infinity,maxHeight:200,alignment: .trailing)
+                           .padding(.top,30)
+                           .padding(.horizontal,15)
+                   }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight: .infinity,alignment: .top)
     }
 }
 

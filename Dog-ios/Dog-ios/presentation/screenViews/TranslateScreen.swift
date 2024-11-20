8d13f11ae71ecb9e5ecd4fb7b1dae58db843d2a1
Muhammad Umair <84626460+muhammadumair64@users.noreply.github.com>
2024-11-20 11:57:49 +0500

    changes

--- Dog-ios/Dog-ios/presentation/screenViews/TranslateScreen.swift
+++ Dog-ios/Dog-ios/presentation/screenViews/TranslateScreen.swift
@@ -11,7 +11,10 @@ import Lottie
 struct TranslateScreen: View {
     @Environment(\.dismiss) var dismiss
     @Binding var navigationPath: NavigationPath
-    @State private var isSwapped = false // State to track swapping
+    
+    @State private var isSwapped = false
+    @State private var isRecording = false
+    
     
     var body: some View {
         ZStack {
@@ -70,27 +73,11 @@ struct TranslateScreen: View {
                     .animation(.easeInOut, value: isSwapped)
                 }.padding(.top,50)
                     .padding(.horizontal, 20)
-                
-                VStack{
-                    Spacer()
-                    
-                    //Text
-                    Text("Tap the record button to Start Recording....\nPress the arrow to switch from do g to human translator and vice versa")
-                        .font(.system(size: 15))
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal,20)
-                        .foregroundColor(.black)
-                    Spacer()
-                    
-                    Image(ImageResource.mic)
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                        .onTapGesture {
-                        }
-                    Spacer()
+                if !isRecording {
+                    micView(isRecording: $isRecording)
+                } else {
+                    RecordingView(isRecording: $isRecording)
                 }
-            
-  
             }
             .frame(maxHeight: .infinity, alignment: .top)
             .padding(.vertical, 20)
@@ -102,4 +89,71 @@ struct TranslateScreen: View {
 
 #Preview {
     TranslateScreen(navigationPath: .constant(NavigationPath()))
+}
+
+struct micView: View {
+    @Binding var isRecording :Bool
+    var body: some View {
+        VStack{
+            Spacer()
+            //Text
+            Text("Tap the record button to Start Recording....\nPress the arrow to switch from do g to human translator and vice versa")
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal,20)
+                .foregroundColor(.black)
+            Spacer()
+            
+            Image(ImageResource.mic)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .onTapGesture {
+                    isRecording = true
+                }
+            Spacer()
+        }
+    }
+}
+
+struct RecordingView: View {
+    @Binding var isRecording: Bool
+    @State private var elapsedTime: Int = 0
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        VStack {
+            LottieView(animation: .named("waves"))
+                .playing()
+                .looping()
+                .frame(height: 250)
+            Spacer()
+            Text("Recording... Tap to stop")
+                .font(.system(size: 15))
+            
+            Spacer()
+            VStack {
+                Image(ImageResource.stopRec)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        isRecording = false
+                    }
+                Text(timeString(from: elapsedTime))
+                    .padding(.top, 10)
+                    .font(.system(size: 18))
+                    .onReceive(timer) { _ in
+                        if isRecording {
+                            elapsedTime += 1
+                        }
+                    }
+            }
+            Spacer()
+        }
+    }
+    
+    private func timeString(from seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
 }
